e6a309501d2036c4fdba2dab04cae4747a8257a8
vaibhav <vvaibhav@gmail.com>
2025-05-26 22:36:31 +0530

    Initial commit

---
+++ Swiper/script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    grid: {
+      rows: 2,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
